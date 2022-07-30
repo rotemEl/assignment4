e52be57b5f668451c9876c92b0e067a2b69c749f
rotemEL <elmaler@bgu.ac.il>
2022-07-30 15:29:37 +0300

    fix reqres

--- static/js/fetch_outer_source.js
+++ static/js/fetch_outer_source.js
@@ -2,7 +2,7 @@ let str1 = 'Hello World!!!:)';
 
 console.log(str1);
 
-function get_outer_user(id) {
+function get_outer_user() {
     var id = document.getElementById("user_id").value;
     fetch('https://reqres.in/api/users/' + id.toString())
     .then(result => result.json())
